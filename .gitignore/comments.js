88ba8105ab2800670c04248a8a071d3c7efacd43
Mahmoud Shaaaban <mshaaban16@outlook.com>
2021-05-24 22:00:32 +0200

    Add comment demo

---
+++ .gitignore/comments.js
@@ -0,0 +1,15 @@
+// this is a demonstratio to the single line comment
+
+/* 
+
+This is a library for implementing xyz
+
+this is version 1.0
+
+
+
+*/
+
+
+// this is a numeric varible
+var num=10;
